145d7cd66fb409bfb90c55f9574889e8fc4e1959
jovan <jovancraft@gmail.com>
2022-10-13 17:38:33 -0500

    finished permutations

--- problems/17-permutations.js
+++ problems/17-permutations.js
@@ -14,6 +14,24 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
 
 const permutations = (array) => {
   // Your code here
+  let empty = [];
+  if (array.length === 0) {
+    return [];
+  }
+  if (array.length === 1) {
+    return [array];
+  }
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+    let whatsLeft = array.slice(0, i).concat(array.slice(i + 1));
+    let variationOfWhatsLeft = permutations(whatsLeft);
+    
+    for (let j = 0; j < variationOfWhatsLeft.length; j++) {
+      let permutedArray = [current].concat(variationOfWhatsLeft[j]);
+      empty.push(permutedArray);
+    }
+  }
+  return empty;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
